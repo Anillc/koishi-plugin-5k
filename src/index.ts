1babe2249ec3e1558184c8009d29b4512f2f9166
Anillc <i@anillc.cn>
2023-01-13 01:42:13 +0800

    add readme

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { Context, Dict, segment } from 'koishi'
+import { Context, Dict, Schema, segment } from 'koishi'
 import { AxiosResponse } from 'axios'
+
+export const Config: Schema<{}> = Schema.object({})
+
+export const name = '5k'
 
 export function apply(ctx: Context) {
   ctx.command('5k <top:string> [bottom:string]', '5000兆円欲しい！', { checkArgCount: true })
     .option('quality', '-q <quality:number> 质量 (1 ~ 100)', { value: 100 })
     .option('alpha', '-a 使用透明通道', { value: false })
     .option('rainbow', '-r 彩虹色', { value: false })
-    .option('single', '-s 只保留下方文字')
-    .before(({ options }, top, bottom) => {
+    .option('single', '-s 只保留下方文字', { value: false })
+    .before(({ options }) => {
       if (options.quality < 1 || options.quality > 100) return '请输入正确的数字。'
     })
     .action(async ({ options }, top, bottom = '欲しい！') => {
@@ -24,6 +28,7 @@ export function apply(ctx: Context) {
           params.single = 'true'
           delete params.bottom
         }
+        // https://github.com/CyberRex0/5000choyen-api
         const img: AxiosResponse<ArrayBuffer> = await ctx.http.axios('https://gsapi.cbrx.io/image', {
           params,
           responseType: 'arraybuffer'
